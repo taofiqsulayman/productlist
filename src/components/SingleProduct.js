ef5de608f07a44f78c3a2506619c66d20e7cddaf
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2022-04-25 21:01:56 +0100

    Linked each product to the specific product page (details and reviews)

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-import { Box, Image, Wrap, WrapItem } from '@chakra-ui/react';
+import { Box, Image, Wrap, WrapItem, Center } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
 
 const SingleProduct = () => {
   const products = useSelector((state) => state.allProducts.products);
@@ -12,39 +13,44 @@ const SingleProduct = () => {
 
     return (
       <WrapItem key={id}>
-      <Box w="260px" borderWidth='1px' borderRadius='xl' overflow='hidden'>
-      <Image w="100%"  h="350px" src={image} alt={title} />
-      <Box p='6'>
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h2'
-          lineHeight='tight'
-          isTruncated
-        >
-          {title}
-        </Box>
+        <Link to={`/product/${id}`}>
+          <Box w="260px" borderWidth='1px' borderRadius='xl' overflow='hidden'>
+          <Center pt='2'>
+            <Image w="80%"  h="350px" src={image} alt={title} />
+          </Center>
+          <Box p='6'>
+            <Box
+              mt='1'
+              fontWeight='semibold'
+              as='h2'
+              lineHeight='tight'
+              isTruncated
+            >
+              {title}
+            </Box>
 
-        <Box>$ {price} </Box>
+            <Box>$ {price} </Box>
 
-        <Box display='flex' mt='2' alignItems='center'>
-          {Array(5)
-            .fill('')
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                color={i < rating.rate ? 'teal.500' : 'gray.300'}
-              />
-            ))}
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {rating.count} reviews
+            <Box display='flex' mt='2' alignItems='center'>
+              {Array(5)
+                .fill('')
+                .map((_, i) => (
+                  <StarIcon
+                    key={i}
+                    color={i < rating.rate ? 'teal.500' : 'gray.300'}
+                  />
+                ))}
+              <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                {rating.count} reviews
+              </Box>
+            </Box>
+
+            <Box> {category} </Box>
+
           </Box>
-        </Box>
-
-        <Box> {category} </Box>
-
-      </Box>
-        </Box>
+          </Box>
+        </Link>
+      
       </WrapItem>
       
 
