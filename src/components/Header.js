3c768f0a550fe7dd874e9c8f6c2b8036bf18ecf0
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2022-04-28 20:11:20 +0100

    add route to home from header and support for ms edge

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import { Box, Heading } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
       <Box p={4} shadow="md">
+      <Link to={'/'} >
         <Heading>Fake Store</Heading>
+      </Link>
       </Box>
     </div>
   )
